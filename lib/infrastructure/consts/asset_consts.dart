feb59b0a64c95fa7730e47c67267503fc781120c
sm-siddarth <siddarthjknair@gmail.com>
2023-04-26 11:15:52 +0530

    Update

--- lib/infrastructure/consts/asset_consts.dart
+++ lib/infrastructure/consts/asset_consts.dart
@@ -7,7 +7,7 @@ class AssetConsts {
 
   ///lottie
   static const lottieSplash = 'assets/lottie/lottieSplash.json';
-  static const lottieSplash1 = 'assets/lottie/lottieSplash1.json';
+  // static const lottieSplash1 = 'assets/lottie/lottieSplash1.json';
 
   ///audio
   static const audioSplash = 'assets/audio/audioSplash.mp3';
